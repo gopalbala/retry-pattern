e97fc28f9bc98c90b9e030c40ab756a4fa54167d
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2021-05-23 00:01:01 +0530

    inital commit

---
+++ src/main/java/com/gb/patterns/retyrpattern/PatternMain.java
@@ -0,0 +1,11 @@
+package com.gb.patterns.retyrpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatternMain {
+    public static void main(String[] args) {
+        SpringApplication.run(PatternMain.class, args);
+    }
+}
